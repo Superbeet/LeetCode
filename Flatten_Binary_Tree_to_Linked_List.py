9ce17f6d22b86922525f711ce689d67b52c48c15
jeffrey <aslan.yeh2010@gmail.com>
2015-11-17 00:28:30 -0800

    Binary Tree Maximum Path Sum ongoing

--- Flatten_Binary_Tree_to_Linked_List.py
+++ Flatten_Binary_Tree_to_Linked_List.py
@@ -5,6 +5,42 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
+
+"""
+Why right_tail firstly? No idea
+"""
+# class Solution(object):
+#     def flatten(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: void Do not return anything, modify root in-place instead.
+#         """
+
+#         def flattenBT(node):
+#             if not node:
+#                 return None
+#             left_tail = flattenBT(node.left)
+#             right_tail = flattenBT(node.right)
+            
+#             if node.left:
+#                 temp = node.right
+#                 node.right = node.left
+#                 node.left = None
+#                 left_tail.right = temp
+
+#             if right_tail:
+#                 return right_tail
+
+#             if left_tail:
+#                 return left_tail
+
+#             return node
+
+#         flattenBT(root)
+
+"""
+off-line preorder transformation with extra space
+"""
 class Solution(object):
     def flatten(self, root):
         """
@@ -34,39 +70,7 @@ class Solution(object):
         self.preorder(node.right, all_nodes)
 
 """
-Why right_tail firstly? No idea
-"""
-class Solution(object):
-    def flatten(self, root):
-        """
-        :type root: TreeNode
-        :rtype: void Do not return anything, modify root in-place instead.
-        """
-
-        def flattenBT(node):
-            if not node:
-                return None
-            left_tail = flattenBT(node.left)
-            right_tail = flattenBT(node.right)
-            
-            if node.left:
-                temp = node.right
-                node.right = node.left
-                node.left = None
-                left_tail.right = temp
-
-            if right_tail:
-                return right_tail
-
-            if left_tail:
-                return left_tail
-
-            return node
-
-        flattenBT(root)
-
-"""
-real-time inorder transformation
+real-time preorder transformation
 """
 class Solution(object):
     def flatten(self, root):
@@ -78,22 +82,88 @@ class Solution(object):
         if not root:
             return
 
-        last_visited_node = [None]
-
-        def flattenBT(node):
+        def flattenBT(node, last_visited_node):
             if node is None:
                 return
-            # right_node = node.right
-            if last_visited_node[0] is not None:
-                last_visited_node[0].left = None
-                last_visited_node[0].right = node
+            # store the orginal right child
+            right_node = node.right
 
+            # modify the right child and set the left child to None
+            if last_visited_node[0] is not None:
+                last_visited_node[0].right = node
+                last_visited_node[0].left = None
+
+            # update last_visited_node
             last_visited_node[0] = node
 
-            flattenBT(node.left)
-            flattenBT(node.right)
+            flattenBT(node.left, last_visited_node)
+            flattenBT(right_node, last_visited_node)
 
-        flattenBT(root)
+        flattenBT(root, [None])
+
+class Solution:
+    """ root's right subtree's predecessor is the rightmost node of its left child  
+    """
+    def flatten(self, root):
+        # eliminate each level's root's left child
+        while root:
+            if root.left:
+                p = root.left
+
+                while p.right: 
+                    p = p.right
+
+                p.right = root.right
+                root.right = root.left
+                root.left = None
+
+            root = root.right
+
+"""
+real-time inorder transformation
+"""
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+
+        def do_flatten(self, node):
+            if node is None:
+                return None
+
+            right_child = root.right
+            new_head = node
+
+            left_tree = do_flatten(node.left)
+
+            if left_tree:   #left_tree!=None
+                new_head = left_tree
+                tail = left_tree.left
+                tail.right = node
+                node.left = tail
+                left_tree.left = root
+
+            right_tree = do_flatten(right_child)
+
+            if right_tree:
+                root.right = right_tree
+                new_head.left = right_tree.left
+                right_tree.left = root
+
+            return new_head
+
+
+
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+
+
 
 root = TreeNode(1)
 root.left = TreeNode(2)
